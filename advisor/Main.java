fa9b784d5d3ff0a90934bf39248414fb3223e876
Andrey <teotihuacans@gmail.com>
2020-05-09 00:15:26 +0300

    08052020

--- advisor/Main.java
+++ advisor/Main.java
@@ -1,5 +1,9 @@
 package advisor;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,18 +19,11 @@ public class Main {
     private static final String API_SUB_LINK = "/api/token";
     private static String access_token = "";
     private static String access_server = "https://accounts.spotify.com";
+    private static String api_server = "https://api.spotify.com";
     private static String auth = "/authorize?" + CLIENT_ID + "&" + REDIRECT_URI;
-
-    private static void print(String vStr, ArrayList<String> vList, boolean isAuth) {
-        if(isAuth) {
-            System.out.println(vStr);
-            for (String out : vList) {
-                System.out.println(out);
-            }
-        } else {
-            System.out.println("Please, provide access for application.");
-        }
-    }
+    private static WebRequest webRq = new WebRequest();
+    private static ArrayList<String> categories = new ArrayList<>();
+    private static ArrayList<String> categoriesId = new ArrayList<>();
 
     private static void doAuth() throws Exception {
         NewHttpServer connection = new NewHttpServer();
@@ -43,70 +40,163 @@ public class Main {
             if (vCode == null || !vCode.startsWith("code=")) {
                 Thread.sleep(100);
             } else {
-                //System.out.println("Response: " + connection.query.toString());
                 System.out.println("code received");
-
                 //new WebRequest().checkSrvRsp();
                 connection.webStop(1);
-
                 Thread.sleep(100);
-
                 System.out.println("making http request for access_token...");
-                WebRequest webRq = new WebRequest();
-                webRq.webGo(access_server + API_SUB_LINK, vCode, CLIENT_ID, CLIENT_SECRET);
-
-                isAuth = true;
+                access_token = webRq.webGetToken(access_server + API_SUB_LINK, vCode, CLIENT_ID, CLIENT_SECRET);
+                if (!access_token.isEmpty()) {
+                    isAuth = true;
+                    //System.out.println("Parsed token: " + access_token);
+                }
                 break;
             }
         } while (true);
     }
 
+    private static boolean checkAuth() {
+        if(isAuth) {
+            return true;
+        } else {
+            System.out.println("Please, provide access for application.");
+            return false;
+        }
+    }
+
+    private static void getCategories() {
+        categories.clear();
+        categoriesId.clear();
+        try {
+            String sCategories = webRq.webApiRequest(api_server + "/v1/browse/categories", access_token);
+            JsonObject jo = JsonParser.parseString(sCategories).getAsJsonObject();
+            JsonObject categoriesObj = jo.getAsJsonObject("categories");
+            for (JsonElement items : categoriesObj.getAsJsonArray("items")) {
+                categories.add(items.getAsJsonObject().get("name").getAsString());
+                categoriesId.add(items.getAsJsonObject().get("id").getAsString());
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean checkResponse(String response) {
+        JsonObject jo = JsonParser.parseString(response).getAsJsonObject();
+        if (jo.has("error")) {
+            JsonObject jerr = jo.getAsJsonObject("error");
+            System.out.println(jerr.get("message").getAsString());
+            return false;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) throws IOException, InterruptedException, Exception {
         //BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
         for (int i = 0; i < args.length; i += 2) {
             if ("-access".equals(args[i])) {
                 access_server = args[i + 1];
+            } else if ("-resource".equals(args[i])) {
+                api_server = args[i + 1];
             }
         }
 
         Scanner input = new Scanner(System.in);
         String command;
 
-        ArrayList<String> featured = new ArrayList<String>();
-        ArrayList<String> news = new ArrayList<String>();
-        ArrayList<String> categories = new ArrayList<String>();
-        ArrayList<String> playlists = new ArrayList<String>();
-
-        news.add("Mountains [Sia, Diplo, Labrinth]");
-        news.add("Runaway [Lil Peep]");
-        news.add("The Greatest Show [Panic! At The Disco]");
-
-        featured.add("Mellow Morning");
-
-        categories.add("Top Lists");
-
-        playlists.add("Walk Like A Badass");
-
+        //System.out.println("---Spotify API Music Advisor app---");
         //System.out.print(">");
 
         while (!"exit".equalsIgnoreCase(command = input.next())) {
             switch (command.toLowerCase()) {
                 case "new":
-                    print("---NEW RELEASES---", news, isAuth);
+                    if (!checkAuth()) {
+                        break;
+                    }
+                    try {
+                        String sNew = webRq.webApiRequest(api_server + "/v1/browse/new-releases", access_token);
+                        if (!checkResponse(sNew)) {
+                            break;
+                        }
+                        JsonObject jo = JsonParser.parseString(sNew).getAsJsonObject();
+                        JsonObject albumsObj = jo.getAsJsonObject("albums");
+                        for (JsonElement items : albumsObj.getAsJsonArray("items")) {
+                            System.out.println(items.getAsJsonObject().get("name").getAsString());
+                            List<String> sArtists = new ArrayList<>();
+                            for (JsonElement artists : items.getAsJsonObject().getAsJsonArray("artists")) {
+                                sArtists.add(artists.getAsJsonObject().get("name").getAsString());
+                            }
+                            System.out.println(sArtists);
+                            JsonObject extUrls = items.getAsJsonObject().getAsJsonObject("external_urls");
+                            System.out.println(extUrls.get("spotify").getAsString() + "\n");
+                        }
+                    } catch (IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 case "featured":
-                    print("---FEATURED---", featured, isAuth);
+                    if (!checkAuth()) {
+                        break;
+                    }
+                    try {
+                        String sFeatured = webRq.webApiRequest(api_server + "/v1/browse/featured-playlists", access_token);
+                        if (!checkResponse(sFeatured)) {
+                            break;
+                        }
+                        JsonObject jo = JsonParser.parseString(sFeatured).getAsJsonObject();
+                        JsonObject albumsObj = jo.getAsJsonObject("playlists");
+                        for (JsonElement items : albumsObj.getAsJsonArray("items")) {
+                            System.out.println(items.getAsJsonObject().get("name").getAsString());
+                            JsonObject extUrls = items.getAsJsonObject().getAsJsonObject("external_urls");
+                            System.out.println(extUrls.get("spotify").getAsString() + "\n");
+                        }
+                    } catch (IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 case "categories":
-                    print("---CATEGORIES---", categories, isAuth);
+                    if (!checkAuth()) {
+                        break;
+                    }
+                    getCategories();
+                    for (String out : categories) {
+                        System.out.println(out);
+                    }
                     break;
                 case "playlists":
-                    input.next();
-                    print("---MOOD PLAYLISTS---", playlists, isAuth);
+                    if (!checkAuth()) {
+                        break;
+                    }
+                    String inCategory = input.next();
+                    if (categories.contains(inCategory)) {
+                        String catId = categoriesId.get(categories.indexOf(inCategory));
+                        try {
+                            String sPlaylists = webRq.webApiRequest(api_server + "/v1/browse/categories/" + catId + "/playlists", access_token);
+                            if (!checkResponse(sPlaylists)) {
+                                break;
+                            }
+                            JsonObject jo = JsonParser.parseString(sPlaylists).getAsJsonObject();
+                            JsonObject albumsObj = jo.getAsJsonObject("playlists");
+                            for (JsonElement items : albumsObj.getAsJsonArray("items")) {
+                                System.out.println(items.getAsJsonObject().get("name").getAsString());
+                                JsonObject extUrls = items.getAsJsonObject().getAsJsonObject("external_urls");
+                                System.out.println(extUrls.get("spotify").getAsString() + "\n");
+                            }
+                        } catch (IOException | InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    } else {
+                        System.out.println("Unknown category name.");
+                    }
                     break;
                 case "auth":
                     doAuth();
-                    System.out.println(isAuth ? "---SUCCESS---" : "Error!");
+                    if (isAuth) {
+                        getCategories();
+                    }
+                    System.out.println(isAuth ? "Success!" : "Error! Not passed token.");
                     break;
                 default:
                     System.out.println("Unknown command '" + command + "'. Please try another one or type 'exit'.");
@@ -114,6 +204,6 @@ public class Main {
             }
             //System.out.print(">");
         }
-        System.out.println("---GOODBYE!---");
+        //System.out.println("---GOODBYE!---");
     }
 }
--- advisor/WebRequest.java
+++ advisor/WebRequest.java
@@ -1,5 +1,8 @@
 package advisor;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -11,7 +14,10 @@ public class WebRequest {
     private HttpRequest request;
     private HttpClient client = HttpClient.newBuilder().build();
 
-    public void webGo(String access_api_server, String code, String id, String secret) throws IOException, InterruptedException {
+    public String webGetToken(String access_api_server, String code, String id, String secret) throws IOException, InterruptedException {
+        String json;
+        String access_token = "";
+        String refresh_token;
 
         request = HttpRequest.newBuilder()
                 .header("Content-Type", "application/x-www-form-urlencoded")
@@ -20,8 +26,29 @@ public class WebRequest {
                 .build();
 
         response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println("response:");
-        System.out.println(response.body());
+        json = response.body();
+            JsonObject jo = JsonParser.parseString(json).getAsJsonObject();
+            if (jo.has("access_token")) {
+                access_token = jo.get("access_token").getAsString();
+                refresh_token = jo.get("refresh_token").getAsString();
+            }
+
+        return access_token;
+    }
+
+    public String webApiRequest(String resource_api_server, String access_token) throws IOException, InterruptedException {
+
+        request = HttpRequest.newBuilder()
+                .header("Authorization", "Bearer " + access_token)
+                .uri(URI.create(resource_api_server))
+                .GET()
+                .build();
+
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        //System.out.println("Api response:");
+        //System.out.println(response.body());
+
+        return response.body();
     }
 
     public void checkSrvRsp() throws IOException, InterruptedException {
